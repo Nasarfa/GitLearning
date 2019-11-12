b6b59ccc6740faca3d208c6c6393286b6ee4547f
Jerome PC <Jerome PC@Jerome>
2019-11-12 18:03:54 +0530

    Git intial

---
+++ Gitlearning/src/github/Pushcodetogit.java
@@ -0,0 +1,34 @@
+package github;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class Pushcodetogit {
+	
+String [][] data =	{{"Admin","admin1"},
+	{"admin","admin123"},
+	{"admin12","admin"},
+	{"Admin","admin123"}};
+	@DataProvider(name="logindata")	
+	public String[][] datadriven() {
+		return data;
+	}
+	@Test(dataProvider = "logindata")
+	public void Loginwebsite(String Uname,String Pwrd) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Jerome PC\\Desktop\\Chrome driver Latest\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		WebElement username = driver.findElement(By.id("txtUsername"));
+		username.sendKeys("Uname");
+		WebElement password = driver.findElement(By.id("txtPassword"));
+		password.sendKeys("Pwrd");
+		WebElement login = driver.findElement(By.id("btnLogin"));
+		login.click();
+				}
+}
